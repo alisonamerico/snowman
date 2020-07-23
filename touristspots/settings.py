9e0e155078be04c8e927e69262ebd00e7ce32705
alisonamerico <alison.americo@gmail.com>
2020-07-23 11:49:25 -0300

    Installed and configured geos

--- touristspots/settings.py
+++ touristspots/settings.py
@@ -43,11 +43,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
     'touristspots.base',
     'touristspots.api',
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
+
 ]
 
 MIDDLEWARE = [
